51be890ef845d0ebd004f1e87b288b63f931cd9e
ckumar3 <46984283+ckumar3@users.noreply.github.com>
2020-07-25 13:34:23 -0400

    inspect command

---
+++ commands/inspect/inspect.js
@@ -0,0 +1,11 @@
+!commands edit !inspect $(eval
+if (q === "") {
+    "Please choose something to inspect"
+} else if (q === "COMMAND1") {
+    "RESPONSE 1"
+} else if (q === "COMMAND2") {
+    "RESPONSE 2"
+} else {
+    "Can not inspect ".concat(q)
+}
+)
